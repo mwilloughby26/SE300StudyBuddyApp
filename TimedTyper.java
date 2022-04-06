dcafa6df372ba3483ea21c2d8315f36b1d4d5d13
chall1015 <93002893+chall1015@users.noreply.github.com>
2022-04-06 15:58:04 -0400

    Update TimedTyper.java

--- TimedTyper.java
+++ TimedTyper.java
@@ -2,18 +2,10 @@ import java.util.ArrayList;
 
 public class TimedTyper
 {
-	ArrayList<String> termSet = new ArrayList<String>();
-	public String newTerm() {
-		return "test test test";
-	}
-	//Let user type in
-	
-	public int[] getIndex()
-	{
-		int[] indices = new int[2];
-		indices[0] = termSet.size();
-		//indices[1] = whereever in the set we are]
-		
-		return indices;
+	studySetIO studySetIO = new studySetIO();
+	String[][] termSet;
+	public String[][] newSet() {
+		termSet = studySetIO.getStudySet();
+		return termSet;
 	}
 }
