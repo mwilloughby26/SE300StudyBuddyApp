f3a894be459d09c2c892223a6dfd13054c2aebb2
chall1015 <93002893+chall1015@users.noreply.github.com>
2022-04-06 15:59:02 -0400

    Update typerGUI2.java

--- typerGUI2.java
+++ typerGUI2.java
@@ -20,19 +20,22 @@ public class typerGUI2 extends BorderPane
 	private TextField userTextField = new TextField();
 	private Label termLabel = new Label();
 	private Label timerLabel = new Label();
+	private Label finishLabel = new Label();
 	private Button submitBT = new Button("Submit");
 	private Label redoLabel = new Label("Incorrect. Please fix the error and resubmit.");
 	private int totalCorrect = 0;
 	private int totalLength = 0;
 	private Label accuracyLabel = new Label();
 	private Label indexLabel = new Label();
-	
+	int index = 0;
+	String term;
+	String[][] termSet;
 	
 	public typerGUI2()
 	{
-		String term = timedTyper.newTerm();
-		termLabel.setText(term);
-		stackPaneChildren.addAll(termLabel, userTextField, timerLabel, submitBT, redoLabel, accuracyLabel, indexLabel);
+		termSet = timedTyper.newSet();
+		setTerm();
+		stackPaneChildren.addAll(termLabel, userTextField, timerLabel, submitBT, redoLabel, accuracyLabel, indexLabel, finishLabel);
 		long startTime = System.nanoTime();
 		StackPane.setAlignment(timerLabel, Pos.TOP_LEFT);
 		StackPane.setAlignment(accuracyLabel, Pos.TOP_LEFT);
@@ -46,7 +49,6 @@ public class typerGUI2 extends BorderPane
 		redoLabel.setTranslateY(100);
 		redoLabel.setVisible(false);
 		indexLabel.setTranslateY(50);
-		//indexLabel.setText(method call then the other stuff :));
 		accuracyLabel.setText("Set accuracy: 0%");
 		
 		Button exitBTN = new Button("EXIT");
@@ -73,27 +75,57 @@ public class typerGUI2 extends BorderPane
 		submitBT.setOnMousePressed(d -> {
 			if (userTextField.getText().equals(term))
 			{
-				userTextField.setStyle("-fx-text-fill: green;");
 				redoLabel.setVisible(false);
+				index++;
+				term = termSet[index][0];
+				setTerm();
+				if (term == null)
+				{
+					long seconds = (long) (System.nanoTime() - startTime) / 1000000000;
+					int acc = (int) (((double) totalCorrect / totalLength) * 100);
+					finishLabel.setText("Congratulations on completing the set! You finished in " + seconds + " seconds with " + acc + "% accuracy! You may exit.");
+					finishLabel.setVisible(true);
+					userTextField.setVisible(false);
+					timerLabel.setVisible(false);
+					termLabel.setVisible(false);
+					submitBT.setVisible(false);
+					accuracyLabel.setVisible(false);
+				};
+				totalCorrect += term.length();
+				totalLength += term.length();
 			} else
 			{
 				userTextField.setStyle("-fx-text-fill: red;");
 				redoLabel.setVisible(true);
+				for (int i = 0; i < term.length(); i++)
+				{
+					try
+					{
+						if (userTextField.getText(i, i + 1).equals(term.substring(i, i + 1)))
+						{
+							totalCorrect += 1;
+						}
+					} catch (Exception e)
+					{
+						break;
+					}
+				}
+				totalLength += term.length();
 			}
 			
-			for (int i = 0; i < userTextField.getText().length(); i++)
-			{
-				if (userTextField.getText(i, i + 1).equals(term.substring(i, i + 1)))
-				{
-					totalCorrect += 1;
-				}
-			}
-			totalLength += term.length();
 			int accuracy = (int) (((double) totalCorrect / totalLength) * 100);
 			accuracyLabel.setText("Set accuracy: " + accuracy + "%");
 			});
 		
 		timeline.getKeyFrames().add(keyframe);
 		timeline.play();
+	}
+	
+	public void setTerm()
+	{
+		userTextField.clear();
+		userTextField.setStyle("-fx-text-fill: black;");
+		term = termSet[index][0];
+		termLabel.setText(term);
 	}
 }
